a5d10d3e0de88879cde9324421d8a2ceaf21509e
chjptc <chenhao199768@gmail.com>
2024-07-23 00:50:36 +0800

    Revert "补充每日活动"
    This reverts commit 6c05aff58fec2b11442d1cc6a239fa0474b7c0b3.

--- module/equipment/fleet_equipment.py
+++ module/equipment/fleet_equipment.py
@@ -6,36 +6,6 @@ from module.ui.page import page_fleet
 
 OCR_FLEET_INDEX = Digit(OCR_FLEET_INDEX, letter=(90, 154, 255), threshold=128, alphabet='123456')
 
-class DailyEquipment(Equipment):
-    equipment_has_take_on = False
-
-    @property
-    def _fleet_daily(self):
-        fleet = self.config.FLEET_DAILY
-        if isinstance(fleet, list):
-            logger.info(f'Multiple daily fleets are set, change equipment only for the first one. fleet: {fleet}')
-            return fleet[0]
-        else:
-            return fleet
-
-    def equipment_take_on(self):
-        if self.config.FLEET_DAILY_EQUIPMENT is None:
-            return False
-        if self.equipment_has_take_on:
-            return False
-
-        self.ui_ensure(page_fleet)
-        self.ui_ensure_index(self._fleet_daily, letter=OCR_FLEET_INDEX, next_button=FLEET_NEXT, prev_button=FLEET_PREV)
-        super().equipment_take_on(enter=FLEET_ENTER, out=FLEET_CHECK, fleet=self.config.FLEET_DAILY_EQUIPMENT)
-        self.equipment_has_take_on = True
-        self.device.sleep(1)
-        return True
-
-    def equipment_take_off(self):
-        if self.config.FLEET_DAILY_EQUIPMENT is None:
-            return False
-        if not self.equipment_has_take_on:
-            return False
 
 class FleetEquipment(EquipmentChange):
     def fleet_enter(self, fleet):

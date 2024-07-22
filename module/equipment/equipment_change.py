36b8ceba497e62788b2c68e7ab1f541cdd543b68
chjptc <chenhao199768@gmail.com>
2024-07-23 00:44:00 +0800

    Revert "补充每日"
    This reverts commit 5b11ce9a460e6ab49a996e67d671a1650e106d3c.

--- module/equipment/equipment_change.py
+++ module/equipment/equipment_change.py
@@ -16,36 +16,6 @@ EQUIPMENT_GRID = ButtonGrid(
 EQUIPMENT_SCROLL = Scroll(EQUIP_SCROLL, color=(247, 211, 66), name='EQUIP_SCROLL')
 SIM_VALUE = 0.90
 
-class DailyEquipment(Equipment):
-    equipment_has_take_on = False
-
-    @property
-    def _fleet_daily(self):
-        fleet = self.config.FLEET_DAILY
-        if isinstance(fleet, list):
-            logger.info(f'Multiple daily fleets are set, change equipment only for the first one. fleet: {fleet}')
-            return fleet[0]
-        else:
-            return fleet
-
-    def equipment_take_on(self):
-        if self.config.FLEET_DAILY_EQUIPMENT is None:
-            return False
-        if self.equipment_has_take_on:
-            return False
-
-        self.ui_ensure(page_fleet)
-        self.ui_ensure_index(self._fleet_daily, letter=OCR_FLEET_INDEX, next_button=FLEET_NEXT, prev_button=FLEET_PREV)
-        super().equipment_take_on(enter=FLEET_ENTER, out=FLEET_CHECK, fleet=self.config.FLEET_DAILY_EQUIPMENT)
-        self.equipment_has_take_on = True
-        self.device.sleep(1)
-        return True
-
-    def equipment_take_off(self):
-        if self.config.FLEET_DAILY_EQUIPMENT is None:
-            return False
-        if not self.equipment_has_take_on:
-            return False
 
 class EquipmentChange(Equipment):
     equip_list = {}
